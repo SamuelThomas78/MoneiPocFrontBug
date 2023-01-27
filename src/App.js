e8c0803e6ea4bb7ff3662be5398b91687de3a0c6
Samuel <gay.thomas.samuel@gmail.com>
2023-01-27 02:39:14 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [paymentId, setPaymentId] = useState(null);
+  const [cardInput, setCardInput] = useState(null);
+
+  const submitPayment = () => {
+    window.monei.createToken(cardInput)
+      .then(function (result) {
+        console.log(result);
+        if (result.error) {
+          // Inform the user if there was an error.
+        } else {
+          // Send the token to MONEI.
+          moneiTokenHandler(result.token);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // Confirm the payment
+  const moneiTokenHandler = (token) => {
+    return window.monei
+      .confirmPayment({ paymentId: paymentId, paymentToken: token })
+      .then(function (result) {
+        // At this moment you can show a customer the payment result
+        // But you should always rely on the result passed to the callback endpoint on your server
+        // to update the order status
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/create-payment`).then(res => {
+      var paymentId = res.data.id;
+      setPaymentId(paymentId);
+      var cardInput = window.monei.CardInput({
+        paymentId: paymentId,
+        accountId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+      });
+      cardInput.render('#container');
+      setCardInput(cardInput);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+      </div>
+      <button onClick={submitPayment}>Payer 1€10</button>
     </div>
   );
 }
